814f39e52d16d68c1939c03c21b35eab49fc283e
Rosa-Palaci <palacibelrose431@gmail.com>
2024-05-25 23:02:16 -0600

    Creando una tabla en sqlite3 con python

--- controller.py
+++ controller.py
@@ -5,5 +5,23 @@ def createDb():
     conn.commit()
     conn.close()
 
+def createTableEstudiantes():
+    conn =sql.connect("estudiantes.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        """CREATE TABLE estudiantes(
+            idEstudiante INTEGER PRIMARY KEY,
+            numero_lista INTEGER,
+            grupo VARCHAR,
+            genero VARCHAR,
+            ciclo_escolar VARCHAR
+        )
+        """
+        
+    )
+    conn.commit()
+    conn.close()
+
 if __name__ == "__main__":
-    createDb()
+    #createDb()
+    createTable()
