b5535c7aa10c9219ee6e2d650265bd2864779d9d
Rosa-Palaci <palacibelrose431@gmail.com>
2024-05-25 23:34:11 -0600

    leer filas de una tabla SQL SELECT

--- controller.py
+++ controller.py
@@ -16,8 +16,7 @@ def createTableEstudiantes():
             genero VARCHAR,
             ciclo_escolar VARCHAR
         )
-        """
-        
+        """      
     )
     conn.commit()
     conn.close()
@@ -30,8 +29,44 @@ def insertRowEstudiantes(idEstudiante, numero_lista, grupo, genero, ciclo_escola
     conn.commit()
     conn.close()
 
+def readRowEstudiantes():
+    conn =sql.connect("estudiantes.db")
+    cursor = conn.cursor()
+    instruccion = f"SELECT * FROM estudiantes"
+    cursor.execute(instruccion)
+    datos = cursor.fetchall()
+    conn.commit()
+    conn.close()
+    print(datos)
+
+def insertRowsEstudiantes(estudiantesLista):
+    conn =sql.connect("estudiantes.db")
+    cursor = conn.cursor()
+    instruccion = f"INSERT INTO estudiantes  VALUES(?, ?, ?, ?, ?)"
+    cursor.executemany(instruccion, estudiantesLista)
+    conn.commit()
+    conn.close()
+
+def readOrdered(field):
+    conn =sql.connect("estudiantes.db")
+    cursor = conn.cursor()
+    instruccion = f"SELECT * FROM estudiantes ORDER BY {field}"
+    cursor.execute(instruccion)
+    datos = cursor.fetchall()
+    conn.commit()
+    conn.close()
+    print(datos)
+
 if __name__ == "__main__":
     #createDb()
     #createTable()
     #insertRowEstudiantes(1, 1, "A", "Femenino", "2023-2024")
-    insertRowEstudiantes(2, 2, "A", "Masculino", "2023-2024")
+    #insertRowEstudiantes(2, 2, "A", "Masculino", "2023-2024")
+    #readRowEstudiantes()
+    estudiantes = {
+        (3, 3, "A", "Masculino", "2023-2024"),
+        (4, 4, "A", "Femenino", "2023-2024"),
+        (5, 5, "A", "Masculino", "2023-2024")
+    }
+    #insertRowsEstudiantes(estudiantes)
+    readOrdered("genero")
